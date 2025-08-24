e1935eebeccfcb4fd07ebee653de19c2c39baa0e
Richeve Bebedor <richeve.bebedor@gmail.com>
2025-08-24 08:37:09 +0800

    feat: Add Homebrew formula and installation support
    Add Homebrew formula for easy installation on macOS and Linux systems

---
+++ Formula/soft-delete.rb
@@ -0,0 +1,17 @@
+class SoftDelete < Formula
+  desc "Safe file deletion utility that moves files to timestamped backup locations"
+  homepage "https://github.com/volkovasystems/soft-delete"
+  url "https://github.com/volkovasystems/soft-delete/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "MIT"
+
+  depends_on "bash" => :recommended
+
+  def install
+    bin.install "bin/soft-delete"
+  end
+
+  test do
+    assert_match "soft-delete version", shell_output("#{bin}/soft-delete --version")
+  end
+end
